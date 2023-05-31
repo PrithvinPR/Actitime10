6308a0fad14866162fe1d44c109fec294587b388
prith <prith@LAPTOP-RV7UN5T4>
2023-05-31 13:02:35 +0530

    P

---
+++ Acitime/src/generic/BaseClass.java
@@ -0,0 +1,45 @@
+package generic;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+public class BaseClass {
+static {
+	System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+}
+	public static WebDriver driver;
+	@BeforeTest
+		public void openBrowser() {
+		Reporter.log("openBrowser",true);
+			driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+	@AfterTest
+	public void closeBrowser() {
+		Reporter.log("closeBrowser",true);
+		driver.close();
+	}
+	@BeforeMethod
+	public void login() throws IOException {
+		Reporter.log("login",true);
+		FileLib f=new FileLib();
+		String url = f.getPropertyData("url");
+		String un = f.getPropertyData("un");
+		String pwd = f.getPropertyData("pwd");
+		driver.get(url);
+		
+	}
+	
+	}
+	
+	
+
